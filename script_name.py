1dd00459fe9855824ff3e8808e63d374a364e125
ukhanskyi <ukhanskyi@gmail.com>
2018-09-10 10:28:01 +0300

    Add files via upload

---
+++ script_name.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- codding:utf-8 -*-
+
+import sys
+
+print('What is you name?')
+Name=input()
+print('What is you surname?')
+Surname=input()
+print('Hello, ' + Surname + ' ' + Name + '!')
