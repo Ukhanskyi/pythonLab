9d7338fdfbbd4bbd01f58758f2296fb78620b765
ukhanskyi <ukhanskyi@gmail.com>
2018-09-10 12:38:20 +0300

    Add files via upload

---
+++ lab3_2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- codding:utf-8 -*-
+
+temperature_far = input('Введіть градуси фаренгейта: ')
+
+celsius=(int(temperature_far)-32)/1.8
+
+print('Градуси цельсія: ' + str(celsius))
