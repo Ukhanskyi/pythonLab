b2ba7d0dfa81c507f05094e4ae7f0f0172906c35
ukhanskyi <ukhanskyi@gmail.com>
2018-10-29 09:09:48 +0200

    Add files via upload

---
+++ lab8_2.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- codding:utf-8 -*-
+
+
+# Дано список з числами, написати функції, що повертають середнє значення і медіану масиву
+
+
+def selection_sort(source: list) -> list:
+	"""Sort your list """
+
+    for i in range(len(source)):
+        min_i = min(source[i:])
+        min_index = source[i:].index(min_i)
+        source[i + min_index] = source[i]
+        source[i] = min_i
+    return source
+
+print(selection_sort([3, 1, 5, 7, 3, 6, 8, 0, 5]))
---
+++ lab8_3.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- codding:utf-8 -*-
+
+
+# Дано список з числами, написати функції, що повертають середнє значення і медіану масиву
+
+
+import functools
+
+def average(source: list) -> float:
+	"""Search average value """
+
+    return functools.reduce((lambda x, y: x + y), source) / len(source)
+
+def median(source: list) -> float:
+	"""Search median """
+
+    number = len(source)
+    if len(source) % 2 == 1:
+        return sorted(source)[number//2]
+    else:
+        return sum(sorted(source)[number//2-1:number//2+1])/2
+
+print(average([3, 1, 6, 7, 3, 6, 8, 5]))
+print(median([3, 1, 6, 7, 3, 6, 8, 5]))
